deecb20259b1b991b93f1e594be205628eee6148
EC2 Default User <ec2-user@ip-172-31-1-26.ap-northeast-1.compute.internal>
2022-09-05 12:44:58 +0000

    [add]

--- yasashiiphp/receive.php
+++ yasashiiphp/receive.php
@@ -1,4 +1,34 @@
-
-<?php 
-  print_r($_POST);
-  echo $_POST["recipe_name"];
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>出力結果</title>
+  </head>
+  <body>
+    <?php 
+//      echo "<pre>";
+//      print_r($_POST);
+//      echo "</pre>";
+      echo htmlspecialchars($_POST['recipe_name'],ENT_QUOTES,'UTF-8');
+      echo "<br>";
+      if ($_POST['category'] === '1') echo "和食";
+      if ($_POST['category'] === '2') echo "中華";
+      if ($_POST['category'] === '3') echo "洋食";
+      echo "<br>";
+      if ($_POST['difficulty'] === '1') {
+        echo "簡単";
+      } elseif ($_POST['difficulty'] === "2") {
+        echo "普通";
+      } else {
+        echo "難しい";
+      }
+      echo "<br>";
+      if (is_numeric($_POST['budget'])) {
+        echo number_format($_POST['budget']);
+      }
+      echo "<br>";
+      echo nl2br(htmlspecialchars($_POST['howto'],ENT_QUOTES, 'UTF-8'));
+      echo "<br>"
+    ?>
+  </body>
+</html>
